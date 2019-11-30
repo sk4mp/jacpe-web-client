00e422147f90d06f413467f05ed5994d972cb6e8
Max <lugimax05@gmail.com>
2019-11-30 04:41:57 +0200

    Updates

---
+++ precache-manifest.8a4ab41c46ae8326e0304820cad788c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565ce1039ecca697d6fe9190f947419c",
+    "url": "/jacpe-web-client/index.html"
+  },
+  {
+    "revision": "61a9317c9060a5234e04",
+    "url": "/jacpe-web-client/static/css/main.96be4d97.chunk.css"
+  },
+  {
+    "revision": "5a437d09f7c65c1e14b2",
+    "url": "/jacpe-web-client/static/js/2.4f4657bd.chunk.js"
+  },
+  {
+    "revision": "61a9317c9060a5234e04",
+    "url": "/jacpe-web-client/static/js/main.d8927c4f.chunk.js"
+  },
+  {
+    "revision": "597ae6d1f05fe0c309f0",
+    "url": "/jacpe-web-client/static/js/runtime-main.0c88c900.js"
+  },
+  {
+    "revision": "c828842d8aed90727d1f5549b4c5d3d2",
+    "url": "/jacpe-web-client/static/media/loading.c828842d.svg"
+  }
+]);
